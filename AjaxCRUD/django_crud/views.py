3895516f78d132847b1c4ddc76cd0c2a2e094a50
Suyen-Shrestha <suyencrestha@gmail.com>
2020-02-07 14:38:35 +0545

    added crud template and javascript

--- AjaxCRUD/django_crud/views.py
+++ AjaxCRUD/django_crud/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic import ListView, View
+from .models import CrudUser
 
-# Create your views here.
+
+class CrudView(ListView):
+    model = CrudUser
+    template_name = 'crud_ajax/crud.html'
+    context_object_name = 'users'
+
+
+class CreateCrudUser(View):
+    def get(self, request):
+        name = request.GET.get('name', None)
+        address = request.GET.get('address', None)
+        age = request.GET.get('age', None)
+
+        obj = CrudUser.objects.create(
+            name=name,
+            address=address,
+            age=age,
+        )
+        user = {
+            'id': obj.id,
+            'name': obj.name,
+            'address': obj.address,
+            'age': obj.age
+
+        }
+        data = {
+            'user': user
+        }
+
+        return JsonResponse(data)
